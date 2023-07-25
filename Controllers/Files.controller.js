d92b85fd956f5423536752044625787604d4dd05
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2023-07-25 09:56:28 +0530

    Added Javascript code

--- Controllers/Files.controller.js
+++ Controllers/Files.controller.js
@@ -27,6 +27,25 @@ FilesRouter.get('/createCustomFile/:content', (request, response, next) => {
   });
 });
 
+// API: https://stackblitzstartersaqsqlc-ac35--5000--bec01ace.local-credentialless.webcontainer.io/files/createCustomFile
+// JAVASCRIPT CODE:
+// fetch('https://stackblitzstartersaqsqlc-ac35--5000--bec01ace.local-credentialless.webcontainer.io/createCustomFile', {
+//     method: "POST", // *GET, POST, PUT, DELETE, etc.
+//     mode: "cors", // no-cors, *cors, same-origin
+//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: "same-origin", // include, *same-origin, omit
+//     headers: {
+//       "Content-Type": "application/json",
+//       // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     redirect: "follow", // manual, *follow, error
+//     referrerPolicy: "same-origin", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//     body: JSON.stringify({
+//         content: "Hello"
+//     }), // body data type must match "Content-Type" header
+//   }).then((response) => response.json()).then((result) => console.log(result)).catch((err) => console.log('ERROR OCCURED', err));
+
+
 FilesRouter.post('/createCustomFile', async (request, response, next) => {
   const { content } = request.body;
   await createFile(content);
