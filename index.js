0cb2f8d05863a308e43d78a577154dd4c66ee386
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2023-07-25 10:37:13 +0530

    Added Body-parser

--- index.js
+++ index.js
@@ -3,14 +3,22 @@
 const express = require('express');
 const HTTP_SERVER = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
+// CONFIGURING CORS
 HTTP_SERVER.use(cors());
 
-// BASIC SERVER CONFIS
+// CONFIGURING BODY-PARSER
+HTTP_SERVER.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+HTTP_SERVER.use(bodyParser.urlencoded({ extended: false }));
+
+// BASIC SERVER CONFIGS
 const port = 5000;
 
 HTTP_SERVER.listen(port, 'localhost', () => {
   console.log('SERVER STARTED IN THE PORT', port);
 });
 
+// INJECTING API SERVER
 HTTP_SERVER.use('/', require('./app'));
